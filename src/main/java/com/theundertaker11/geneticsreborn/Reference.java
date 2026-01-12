8414f8b3a4efdbda57f0c56bb235b4f797d4a0f9
ACGaming <4818419+ACGaming@users.noreply.github.com>
2026-01-12 15:23:48 +0100

    Enable config GUI
    Fix TheUnderTaker11/GeneticsReborn#239

--- src/main/java/com/theundertaker11/geneticsreborn/Reference.java
+++ src/main/java/com/theundertaker11/geneticsreborn/Reference.java
@@ -8,6 +8,7 @@ public class Reference {
 	public static final String ACCEPTED_MINECRAFT = "[1.12,1.12.2]";
 	public static final String CLIENTPROXY = "com.theundertaker11.geneticsreborn.proxy.ClientProxy";
 	public static final String SERVERPROXY = "com.theundertaker11.geneticsreborn.proxy.CommonProxy";
+	public static final String GUIFACTORY = "com.theundertaker11.geneticsreborn.util.GuiFactory";
 
 	private Reference() {
 		throw new IllegalAccessError("Ref class");
