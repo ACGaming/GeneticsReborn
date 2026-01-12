35da83c19cc51755f90014ee13d232629ea035c7
ACGaming <4818419+ACGaming@users.noreply.github.com>
2026-01-12 12:27:22 +0100

    Update project infrastructure

--- src/main/java/com/theundertaker11/geneticsreborn/Reference.java
+++ src/main/java/com/theundertaker11/geneticsreborn/Reference.java
@@ -2,8 +2,8 @@ package com.theundertaker11.geneticsreborn;
 
 public class Reference {
 
-	public static final String MODID = "geneticsreborn";
-	public static final String VERSION = "1.28";
+	public static final String MODID = Tags.MOD_ID;
+	public static final String VERSION = Tags.VERSION;
 	public static final String NAME = "Genetics Reborn";
 	public static final String ACCEPTED_MINECRAFT = "[1.12,1.12.2]";
 	public static final String CLIENTPROXY = "com.theundertaker11.geneticsreborn.proxy.ClientProxy";
